d263f56b6e962a7fb5d50084dfba063f62161e4d
tin-aquino <kristine.aquino.1230@gmail.com>
2017-02-24 23:21:15 +0800

    webtoon upload

--- view/employee/profile.php
+++ view/employee/profile.php
@@ -40,7 +40,7 @@
   		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 	      	<ul class="nav navbar-nav">
 	      		<li><a href="profile.php">Profile</a></li>
-				<li><a href="webtoons.php">Webtoons</a></li>			
+				<li><a href="upload_webtoon.php">Webtoons</a></li>			
 				<li><a href="survey.php">Survey</a></li>			
 	        </ul>
 	        <ul class="nav navbar-nav navbar-right">
