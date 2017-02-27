20c27e8808c17980a59a53ff70183a1c9b021a00
raphaelsevilla <raphaelsevilla@gmail.com>
2017-02-27 14:12:52 +0800

    resources -> file includes | captcha -> register modal

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 	</script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-	<link rel="stylesheet" type="text/css" href="view/css/index.css">
+	<link rel="stylesheet" type="text/css" href="file_includes/css/index.css">
 	
 	
 </head>
@@ -104,7 +104,6 @@
 						<button type='submit' class="btn btn-default" name='login' value='Login'>
 						Login
 						</button>
-						</div>	
 					</form>
 		      	</div>
 		    	</div>
@@ -137,7 +136,7 @@
 				        <h4 class="modal-title" id="myModalLabel">Register</h4>
 		      		</div>
 		      	<div class="modal-body">
-		        	<form action='controller/register_controller.php' method='POST'>
+		        	<form action='controller/add_user_controller.php' method='POST'>
 		        		<div class="form-group">
 						    <label for="exampleInputEmail1">Last Name</label>
 						    <input type="text" name="lname" class="form-control" id="exampleInputEmail1" placeholder="Last Name" required>
@@ -185,8 +184,19 @@
 						    <label for="exampleInputEmail1">Email address</label>
 						    <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="someone@email.com" required>
 						</div>
+						<div class="form-group">
+						    <label for="exampleInputEmail1">Username</label>
+						    <input type="text" name="username" class="form-control"  id="exampleInputEmail1"  required>
+						</div>
+						<div class="form-group">
+						    <label for="exampleInputPassword1">Password</label>
+						    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
+						</div>
+						<div class="form-group g-recaptcha" data-sitekey="6Lf57hAUAAAAAFcemwhoWxJKs611f0s25TunkbDG">
+							
+						</div>
 						<div class="form-group">    
-						<button type="submit" value="Submit" class="btn btn-default">Submit</button>
+						<button type="submit" value="Submit" class="btn btn-default" name="register">Submit</button>
 						</div>   
 						
     
@@ -209,13 +219,13 @@
 							</select> <br>
 						Email: <input type='email' name='email' placeholder='someone@gmail.com' required> <br>
 						<input type='submit' value='Submit'>
-						-->
+						
 					</form>
 		      	</div>
 		    	</div>
 		  	</div>
 		</div>
-<!--
+
 		<div id='register'>
 			<h2>Register</h2>
 			<form action='controller/add_user_controller.php' method='POST'>
@@ -227,37 +237,32 @@
 						<input type='radio' name='sex' value='F'> Female <br>
 				City: 	
 					<select name='city'>
-						<option value=''>--Choose a city--</option>
+						<option value=''>Choose a city</option>
 						<?php citylist(); ?>
 					</select> <br>
 				Province: 
 					<select name="province">
-						<option value=''>--Choose a province--</option>
+						<option value=''>Choose a province</option>
 						<?php provincelist(); ?>
 					</select> <br>
 				Email: <input type='email' name='email' placeholder='someone@gmail.com' required> <br>
 				Username: <input type='text' name='username' placeholder='Username' required> <br>
 				Password: <input type='text' name='password' placeholder='Password' required> <br>
-				<!-- RECAPTCHA -->
+				//RECAPTCHA
 				<div class="g-recaptcha" data-sitekey="6Lf57hAUAAAAAFcemwhoWxJKs611f0s25TunkbDG"></div>
-				<!-- END RECAPTCHA -->
-				<input type='submit' value='Submit' name='register'>
-			</form>
+				//END RECAPTCHA
+				<input type='submit' value='Submit' name='register'>-->
+					</form>
+				</div>
+				</div>
+			</div>
 		</div>
-	</div>
 
-	<div id='menu'>
-		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="view/about.php">About</a></li>
-			<li><a href="view/wt_list.php">Webtoons List</a></li>
-		</ul>		
-	</div>
--->	<div class="container">
+	<div class="container">
 	<div class="row">
 		  <div class="col-sm-6 col-md-4">
 		    	<div class="thumbnail">
-		      		<img src="view/images/1.jpg" alt="...">
+		      		<img src="file_includes/images/1.jpg" alt="...">
 		      		<div class="caption">
 				        <h3>Thumbnail label</h3>
 				        <p>...</p>
