8d4cb92105fa10ad6197022abce239bb7035ebe1
raphaelsevilla <raphaelsevilla@gmail.com>
2017-03-03 13:53:46 +0800

    forgot password

--- view/change_password.php
+++ view/change_password.php
@@ -12,26 +12,34 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Forgot Password</title>
+	<script
+	src="https://code.jquery.com/jquery-3.1.1.min.js"
+	integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+	crossorigin="anonymous">	
+	</script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-	<div id='forgot_pass'>
-		<h4>Forgot Password</h4>
-		<form action="../controller/forgot_pass_controller.php" method='POST'>
-			<form action="../../controller/update_password_controller.php" method="POST">
-			<table>
-				<tr>
-					<td>New Password: </td>
-					<td><input type='password' name='newpass1' required></td>
-				</tr>
-				<tr>
-					<td>Re-type New Password: </td>
-					<td><input type='password' name='newpass2' required></td>
-				</tr>
-				<tr>
-					<td><input type='submit' name='change_pass' value='Submit'></td>				
-				</tr>
-			</table>
-		</form>
+	
+	<div style="padding: 50px; width: 800px;">
+		<h2>Change Password</h2>
+		<form action="../controller/forgot_pass_controller.php" method="POST">
+			<form action='../../controller/update_password_controller.php' method='POST'>
+				<div class="form-group">
+				    <label for="exampleInputPassword1">New Password</label>
+				    <input type="password" name="newpass1" class="form-control" id="exampleInputPassword1"placeholder="New Password" required>
+				</div>
+				<div class="form-group">
+				    <label for="exampleInputPassword1">Re-type New Password</label>
+				    <input type="password" name="newpass2" class="form-control" id="exampleInputPassword1"placeholder="Re-type New Password" required>
+				</div>
+				<div class="form-group">
+					<button type='submit' class="btn btn-default" name='change_pass' value='Submit'>
+						Submit
+					</button>
+				</div>
+			</form>
 		</form>
 	</div>
 </body>
--- view/show_secQ.php
+++ view/show_secQ.php
@@ -12,17 +12,45 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Forgot Password</title>
+	<script
+	src="https://code.jquery.com/jquery-3.1.1.min.js"
+	integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+	crossorigin="anonymous">	
+	</script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-	<div id='forgot_pass'>
-		<h4>Forgot Password</h4>
-		<form action="../controller/forgot_pass_controller.php" method='POST'>
-			Security Question 1: <?php echo get_secQ($_SESSION['accountID'], 'q1'); ?> <br>
-			Answer 1: <input ='type' name='a1' placeholder='Answer' required> <br>
-			Security Question 2: <?php echo get_secQ($_SESSION['accountID'], 'q2'); ?> <br>
-			Answer 2: <input ='type' name='a2' placeholder='Answer' required> <br>
-			<input type='submit' name='showsecQ' value='Submit'>
-			<td><a href="../index.php">Cancel</a></td>	
+	<div style="padding: 50px; width: 800px;">
+
+		<h2>Forgot Password</h2>
+		<blockquote>
+			<p>Please answer the questions to proceed.</p>
+		</blockquote>
+		<form action='../controller/forgot_pass_controller.php' method='POST'>
+			<div class="form-group">
+				<label for="exampleInputEmail1">Security Question 1</label>
+				<p class="form-control-static"><?php echo get_secQ($_SESSION['accountID'], 'q1'); ?></p>
+			</div>
+			<div class="form-group">
+			    <label for="exampleInputEmail1">Answer 1</label>
+			    <input type="text" class="form-control" name="a1" id="exampleInputEmail1" required>
+			</div>
+			<div class="form-group">
+				<label for="exampleInputEmail1">Security Question 2</label>
+				<p class="form-control-static"><?php echo get_secQ($_SESSION['accountID'], 'q2'); ?></p>
+			</div>
+			<div class="form-group">
+			    <label for="exampleInputEmail1">Answer 2</label>
+			    <input type="text" class="form-control" name="a2" id="exampleInputEmail1"  required>
+			</div>
+			
+			<div class="form-group">
+			<button type='submit' class="btn btn-default" name='showsecQ' value='Submit'>
+			Submit
+			</button>
+			<a href="../index.php" class="btn btn-danger">Cancel</a>
+			</div>
 		</form>
 	</div>
 </body>
