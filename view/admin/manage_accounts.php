9ff1bb6584b918957adfcb74fbed211f6628eb82
raphaelsevilla <raphaelsevilla@gmail.com>
2017-03-03 11:00:15 +0800

    admin side - backing up employee

--- view/admin/manage_accounts.php
+++ view/admin/manage_accounts.php
@@ -15,8 +15,50 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Admin | Manage Accounts </title>
+	<script
+	src="https://code.jquery.com/jquery-3.1.1.min.js"
+	integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+	crossorigin="anonymous">	
+	</script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
+
+	<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+		    <div class="navbar-header">
+		      	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+		      	</button>
+		      	<a class="navbar-brand" href="#">Brand</a>
+		    </div>
+	  		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      	<ul class="nav navbar-nav">		      		
+					<li><a href="profile.php">Profile</a></li>
+					<li><a href="manage_accounts.php?users=all">Manage Accounts</a></li>		
+					<li><a href="users_list.php">Users List</a></li>		
+		        </ul>
+		        <ul class="nav navbar-nav navbar-right">
+		        	<li class="dropdown">
+		        		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Manage Account <span class="caret"></span></a>
+				        <ul class="dropdown-menu">
+				            <li><a href="#" data-toggle="modal" data-target="#myUpdateAccount">Update Account Information</a></li>
+				            <li><a href="#" data-toggle="modal" data-target="#myUpdateSecurityQ">Update Security Questions</a></li>
+				            <li><a href="#" data-toggle="modal" data-target="#myUpdatePassword">Update Password</a></li>
+				            <li role="separator" class="divider"></li>
+				            <li><a href="../../controller/logout_controller.php">Logout</a></li>
+				        </ul>
+				    </li>
+		        	<!--<li><a href="../../controller/logout_controller.php">Logout</a></li>-->
+		        </ul>
+		        
+		    </div>   	
+		</nav>
+	<!--	
 	<div id='menu'>
 		<ul>
 			<li><a href="profile.php">Profile</a></li>
@@ -24,9 +66,34 @@
 			<li><a href="users_list.php">Users List</a></li>			
 			<li><a href="../../controller/logout_controller.php">Logout</a></li>
 		</ul>
-	</div>
-	
+	</div>-->
+	<div class="container">
 	<!-- convert to dropdown menu -->
+	<div class="dropdown">
+	  	<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+	    Dropdown
+	    	<span class="caret"></span>
+	  	</button>
+	  	<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+	  		<li><a href="manage_accounts.php?users=all" type="submit" name='all' value='All'>All</a></li>
+	  		<li><a href="manage_accounts.php?users=admin" type="submit" name='admin' value='Admin'>Admin</a></li>
+	  		<li><a href="manage_accounts.php?users=employee" type="submit" name='employee' value='Employee'>Employee</a></li>
+	  		<li><a href="manage_accounts.php?users=active" type="submit" name='active' value='Active'>Active</a></li>
+	  		<li><a href="manage_accounts.php?users=inactive" type="submit" name='inactive' value='Inactive'>Inactive</a></li>
+
+	    	<!--<li><form action="manage_accounts.php?users=all" method='POST'>
+					<input type="submit" name='all' value='All'></form></li>
+	    	<li><form action="manage_accounts.php?users=admin" method='POST'>
+					<input type="submit" name='admin' value='Admin'></form></li>
+		    <li><form action="manage_accounts.php?users=employee" method='POST'>
+					<input type="submit" name='employee' value='Employee'></form></li>
+		    <li><form action="manage_accounts.php?users=active" method='POST'>
+					<input type="submit" name='active' value='Active'></form></li>
+			<li><form action="manage_accounts.php?users=inactive" method='POST'>
+					<input type="submit" name='inactive' value='Inactive'></form></li>	-->	
+	  	</ul>
+	</div>
+	<!--
 	<div id='dropdown_list'>
 		<h4>Dropdown Menu</h4>
 		<ul>
@@ -52,10 +119,10 @@
 			</li>
 		</ul>
 
-	</div>
+	</div>-->
 
 	<div id='accounts_list'>
-		<table>
+		<table class="table table-hover">
                <tr>
                    <th>#</th>
                    <th>Employee Name</th>
@@ -219,6 +286,7 @@
 				</tr>				
 			</table>
 		</form>
+	</div>
 	</div>
 </body>
 </html>
