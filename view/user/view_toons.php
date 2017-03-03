8f1096be02a60eed9a3d68c096b8a9830f7f4c6b
tin-aquino <kristine.aquino.1230@gmail.com>
2017-03-03 15:21:13 +0800

    User: remove favorites

--- view/user/view_toons.php
+++ view/user/view_toons.php
@@ -43,7 +43,6 @@
 	  		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 		      	<ul class="nav navbar-nav">
 		      		<li><a href="index.php">Home</a></li>
-					<li><a href="#">Favorites</a></li>			
 					<li><a href="webtoons.php">Webtoon</a></li>			
 		        </ul>
 		        <ul class="nav navbar-nav navbar-right">
